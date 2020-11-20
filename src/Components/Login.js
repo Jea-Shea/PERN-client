923a6c3ac5b5de544dbe829542e9225a50114032
aaronurbanski <aaronurbanski1@gmail.com>
2020-11-20 18:48:17 -0500

    styling updates

--- src/Components/Login.js
+++ src/Components/Login.js
@@ -14,42 +14,42 @@ const useStyles = makeStyles((theme) => ({
 const Login = (props) => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-  const [passwordValid, setPasswordValid] = useState(false);
-  const [emailValid, setEmailValid] = useState(false);
+  const [emailError, setEmailError] = useState(true);
   useEffect(() => {
-    setPasswordValid(true);
-  }, [password]);
-  useEffect(() => {
-    setEmailValid(/.*@\..*/.test(email));
+    setEmailError(!/.+@.+\..{2}.*/.test(email));
   }, [email]);
   let handleSubmit = (event) => {
     event.preventDefault();
-    fetch("http://localhost:8080/user/login", {
-      method: "POST",
-      body: JSON.stringify({
-        user: { email: email, passwordHash: password },
-      }),
-      headers: new Headers({
-        "Content-Type": "application/json",
-      }),
-    })
-      .then((response) => response.json())
-      .then((data) => props.updateToken(data.sessionToken));
+    if (!emailError && email && password) {
+      fetch("http://localhost:8080/user/login", {
+        method: "POST",
+        body: JSON.stringify({
+          user: { email: email, passwordHash: password },
+        }),
+        headers: new Headers({
+          "Content-Type": "application/json",
+        }),
+      })
+        .then((response) => response.json())
+        .then((data) => props.updateToken(data.sessionToken));
+    }
   };
   return (
     <div>
-      <h1>Login</h1>
+      <h1>LOGIN</h1>
       <form onSubmit={handleSubmit}>
         <div>
           <TextField
-            error={emailValid}
+            error={emailError}
             id="email"
             label="Email"
             onChange={(e) => setEmail(e.target.value)}
+            helperText={emailError && email ? "Invalid email address" : ""}
           />
         </div>
         <div>
           <TextField
+            error={password.length < 8}
             id="password"
             label="Password"
             type="password"
@@ -57,9 +57,13 @@ const Login = (props) => {
           />
         </div>
         <div>
-          <IconButton type="submit">
-            <ForwardIcon color="primary" style={{ fontSize: 40 }} />
-          </IconButton>
+          {emailError || password.length < 8 ? (
+            ""
+          ) : (
+            <IconButton type="submit">
+              <ForwardIcon color="primary" style={{ fontSize: 40 }} />
+            </IconButton>
+          )}
         </div>
       </form>
     </div>
--- src/Components/Signup.js
+++ src/Components/Signup.js
@@ -11,59 +11,116 @@ const useStyles = makeStyles((theme) => ({
     },
   },
 }));
-const Login = (props) => {
+const Signup = (props) => {
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [nameError, setNameError] = useState(true);
+  const [emailError, setEmailError] = useState(true);
+  const [passwordError, setPasswordError] = useState(true);
+  useEffect(() => {
+    setNameError(!/[\w]+/.test(name));
+  }, [name]);
+  useEffect(() => {
+    setEmailError(!/.+@.+\..{2}.*/.test(email));
+  }, [email]);
+  useEffect(() => {
+    if (
+      password.length > 7 &&
+      /.*[a-z]/.test(password) &&
+      /.*[A-Z]/.test(password) &&
+      /.*[0-9]/.test(password) &&
+      /.*[!@#\$\%\^\&\*\(\),\<\.\>\[\]\{\}\?\/~\|'";:]/.test(password)
+    ) {
+      setPasswordError(false);
+    } else {
+      setPasswordError(true);
+    }
+  }, [password]);
 
   let handleSubmit = (event) => {
-    event.preventDefault();
-    fetch("http://localhost:8080/user/signup", {
-      method: "POST",
-      body: JSON.stringify({
-        user: { name: name, email: email, passwordHash: password },
-      }),
-      headers: new Headers({
-        "Content-Type": "application/json",
-      }),
-    })
-      .then((response) => response.json())
-      .then((data) => props.updateToken(data.sessionToken));
+    if (
+      !emailError &&
+      !passwordError &&
+      !nameError &&
+      name &&
+      email &&
+      password
+    ) {
+      event.preventDefault();
+      fetch("http://localhost:8080/user/signup", {
+        method: "POST",
+        body: JSON.stringify({
+          user: { name: name, email: email, passwordHash: password },
+        }),
+        headers: new Headers({
+          "Content-Type": "application/json",
+        }),
+      })
+        .then((response) => response.json())
+        .then((data) => props.updateToken(data.sessionToken));
+    }
   };
   return (
     <div>
-      <h1>Signup</h1>
+      <h1>SIGNUP</h1>
       <form onSubmit={handleSubmit}>
         <div>
           <TextField
+            error={nameError}
             id="name"
             label="Name"
             onChange={(e) => setName(e.target.value)}
+            helperText={nameError && name ? "Invalid name" : ""}
           />
         </div>
         <div>
           <TextField
+            error={emailError}
             id="email"
+            type="email"
             label="Email"
             onChange={(e) => setEmail(e.target.value)}
+            helperText={emailError && email ? "Invalid email address" : ""}
           />
         </div>
         <div>
           <TextField
+            error={passwordError}
             id="password"
             label="Password"
             type="password"
             onChange={(e) => setPassword(e.target.value)}
+            helperText={
+              passwordError && password ? (
+                <div>
+                  <div>Password must be 8 characters or longer,</div>
+                  <div>contain upper- & lower-case letters,</div>
+                  <div>a number, and a special character</div>
+                </div>
+              ) : (
+                ""
+              )
+            }
           />
         </div>
         <div>
-          <IconButton type="submit">
-            <PersonAddIcon color="primary" style={{ fontSize: 40 }} />
-          </IconButton>
+          {!emailError &&
+          !passwordError &&
+          !nameError &&
+          name &&
+          email &&
+          password ? (
+            <IconButton type="submit">
+              <PersonAddIcon color="primary" style={{ fontSize: 40 }} />
+            </IconButton>
+          ) : (
+            ""
+          )}
         </div>
       </form>
     </div>
   );
 };
 
-export default Login;
+export default Signup;
